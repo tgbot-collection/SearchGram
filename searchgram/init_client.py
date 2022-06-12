eb930da0e501f8f257653bfc8409426724137716
BennyThink <benny.think@gmail.com>
2022-06-12 14:02:14 +0800

    fix PROXY in docker env

--- searchgram/init_client.py
+++ searchgram/init_client.py
@@ -7,17 +7,23 @@
 
 __author__ = "Benny <benny.think@gmail.com>"
 
+import json
+
 from pyrogram import Client
 
 from config import APP_HASH, APP_ID, PROXY
 
 
 def get_client(token=None):
+    if isinstance(PROXY, str):
+        proxy = json.loads(PROXY)
+    else:
+        proxy = PROXY
     if token:
         return Client("session/bot", APP_ID, APP_HASH, bot_token=token,
-                      proxy=PROXY
+                      proxy=proxy
                       )
     else:
         return Client("session/client", APP_ID, APP_HASH,
-                      proxy=PROXY
+                      proxy=proxy
                       )
