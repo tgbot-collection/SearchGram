e33743b3b20945ffa506b789ba00fc7d2959d80f
Benny <benny.think@gmail.com>
2023-05-13 14:58:38 +0200

    add tqdm

--- searchgram/add_timestamp.py
+++ searchgram/add_timestamp.py
@@ -7,6 +7,8 @@
 import logging
 import time
 
+from tqdm import tqdm
+
 from engine import SearchEngine
 
 logging.basicConfig(level=logging.INFO)
@@ -17,18 +19,20 @@ def convert_to_timestamp(date: str) -> int:
     return int(time.mktime(time.strptime(date, "%Y-%m-%d %H:%M:%S")))
 
 
-limit = 20
+limit = 100
 offset = 0
 index = s.client.index("telegram")
 total_documents = index.get_stats().number_of_documents
 
-for offset in range(0, total_documents, limit):
-    documents = index.get_documents({"limit": limit, "offset": offset}).results
-    for res in documents:
-        date_string = res.date
-        timestamp = convert_to_timestamp(date_string)
-        ID = res.ID
-        uid = s.client.index("telegram").update_documents({"ID": ID, "timestamp": timestamp})
-        logging.info("Updated %s - %s", res.ID, uid.task_uid)
+logging.info("Starting to add timestamp to documents")
+
+with tqdm(total=total_documents) as pbar:
+    for offset in range(0, total_documents, limit):
+        documents = index.get_documents({"limit": limit, "offset": offset}).results
+        for res in documents:
+            date_string = res.date
+            timestamp = convert_to_timestamp(date_string)
+            uid = s.client.index("telegram").update_documents({"ID": (res.ID), "timestamp": timestamp})
+            pbar.update(1)
 
 logging.info("Done")
